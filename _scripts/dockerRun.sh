cd3121946b48a7edf6ebd0c246f1117f47fb6ebd
Paul Jolly <paul@myitcv.io>
2019-03-30 06:24:55 +0000

    _scripts: fix check for formatted go code (#49)
    We don't need it to be environment-variable driven for now

--- _scripts/dockerRun.sh
+++ _scripts/dockerRun.sh
@@ -12,5 +12,5 @@ go mod tidy
 # https://github.com/golang/go/issues/27868#issuecomment-431413621
 go list all > /dev/null
 
-if [[ -n $CHECK_GOFMT ]]; then diff <(echo -n) <(gofmt -d .); fi
+diff <(echo -n) <(gofmt -d $(git ls-files '**/*.go' '*.go' | grep -v cmd/govim/internal))
 test -z "$(git status --porcelain)" || (git status; git diff; false)
