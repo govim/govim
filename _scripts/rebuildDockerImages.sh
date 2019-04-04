c7876c58a9871a4f9b6de8ce2a993edc73449096
Paul Jolly <paul@myitcv.io>
2019-04-04 16:16:58 +0100

    ci: add testing of older vim versions

--- _scripts/rebuildDockerImages.sh
+++ _scripts/rebuildDockerImages.sh
@@ -5,4 +5,7 @@ set -eu
 vbashPath=$(realpath --relative-to=$PWD $(gobin -m -p github.com/myitcv/vbash))
 gobinPath=$(realpath --relative-to=$PWD $(gobin -m -p github.com/myitcv/gobin))
 
-docker build --build-arg "GOBINPATH=$gobinPath" --build-arg "VBASHPATH=$vbashPath" --build-arg "GO_VERSION=$GO_VERSION" --build-arg "VIM_VERSION=$VIM_VERSION" -t myitcv/govim:${GO_VERSION}_${VIM_VERSION} .
+for i in $(echo $VIM_VERSIONS | tr ',' '\n')
+do
+	docker build --build-arg "GOBINPATH=$gobinPath" --build-arg "VBASHPATH=$vbashPath" --build-arg "GO_VERSION=$GO_VERSION" --build-arg "VIM_VERSION=$i" -t myitcv/govim:${GO_VERSION}_${i} .
+done
