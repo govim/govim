7e8565512cc25532e8decfdcc7499c02d18ba3cd
Paul Jolly <paul@myitcv.io>
2019-04-04 16:30:20 +0100

    ci: add testing of older vim versions (#78)

--- _scripts/buildGovimImage.sh
+++ _scripts/buildGovimImage.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env vbash
 
+if [ "$#" -gt 1 ]
+then
+	echo "We take at most one argument"
+	exit 1
+fi
+
+if [ "$#" -eq 1 ]
+then
+	VIM_VERSION="$1"
+else
+	VIM_VERSION=$(echo $VIM_VERSIONS | tr ',' '\n' | tail -n 1)
+fi
+
+export VIM_VERSION
+
 cat Dockerfile.user | envsubst '$GO_VERSION,$VIM_VERSION' | docker build -t govim --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g $USER) -
