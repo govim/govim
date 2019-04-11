2da0ecb87b3d6798e461829708199c04f564af46
Paul Jolly <paul@myitcv.io>
2019-04-11 15:43:08 +0100

    ci: add support for multiple Go versions

--- _scripts/buildGovimImage.sh
+++ _scripts/buildGovimImage.sh
@@ -13,11 +13,8 @@ then
 	VIM_VERSION="$1"
 	GO_VERSION="$2"
 else
-	if [ "${VIM_VERSION:-}" == "" ]
-	then
-		VIM_VERSION=$(echo $VIM_VERSIONS | tr ',' '\n' | tail -n 1)
-	fi
-	# $GO_VERSION used as is
+	VIM_VERSION=$(echo $VIM_VERSIONS | tr ',' '\n' | tail -n 1)
+	GO_VERSION=$(echo $GO_VERSIONS | tr ',' '\n' | tail -n 1)
 fi
 
 cat Dockerfile.user | GO_VERSION=$GO_VERSION VIM_VERSION=$VIM_VERSION envsubst '$GO_VERSION,$VIM_VERSION' | docker build -t govim --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g $USER) -
