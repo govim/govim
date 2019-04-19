78f6ac0205bd2e207da5064805b4af7e11b8f2e4
Paul Jolly <paul@myitcv.io>
2019-04-19 14:14:39 +0100

    cmd/govim: handle buffer delete event
    Otherwise we will potentially end up with stale data in govim

--- cmd/govim/main.go
+++ cmd/govim/main.go
@@ -150,6 +150,7 @@ func (g *govimplugin) Init(gg govim.Govim, errCh chan error) error {
 	g.DefineCommand(string(config.CommandGoToPrevDef), g.gotoPrevDef, govim.NArgsZeroOrOne, govim.CountN(1))
 	g.DefineFunction(string(config.FunctionHover), []string{}, g.hover)
 	g.DefineAutoCommand("", govim.Events{govim.EventCursorHold, govim.EventCursorHoldI}, govim.Patterns{"*.go"}, false, g.updateQuickfix)
+	g.DefineAutoCommand("", govim.Events{govim.EventBufDelete}, govim.Patterns{"*.go"}, false, g.deleteCurrentBuffer, "eval(expand('<abuf>'))")
 
 	g.isGui = g.ParseInt(g.ChannelExpr(`has("gui_running")`)) == 1
 
