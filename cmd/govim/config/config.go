1f3d34f5f427276134df5a93910f41755c337a93
Paul Jolly <paul@myitcv.io>
2019-04-23 10:44:31 +0100

    cmd/govim: make GoToDef and GoToPrevDef respect switchbuf (#166)

--- cmd/govim/config/config.go
+++ cmd/govim/config/config.go
@@ -7,21 +7,18 @@ const (
 	// to use for formatting on save.  Options are given by constants of type
 	// FormatOnSave
 	GlobalFormatOnSave = GlobalPrefix + "format_on_save"
-
-	// GlobalGoToDefMode is a string value variable that configures how
-	// CommandGoToDef should behave. See GoToDefMode-typed constant values for
-	// more information.
-	GlobalGoToDefMode = GlobalPrefix + "go_to_def"
 )
 
 type Command string
 
 const (
 	// CommandGoToDef jumps to the definition of the identifier under the cursor,
-	// pushing the current location onto the jump stack.
+	// pushing the current location onto the jump stack. CommandGoToDef respects
+	// &switchbuf
 	CommandGoToDef Command = "GoToDef"
 
 	// CommandGoToPrevDef jumps to the previous location in the jump stack.
+	// CommandGoToPrevDef respects &switchbuf
 	CommandGoToPrevDef Command = "GoToPrevDef"
 
 	// CommandHello is a friendly command, largely for checking govim is
@@ -66,23 +63,4 @@ const (
 	// FormatOnSaveGoImports specifies that gopls should run a goimports-based
 	// formatting on a .go file before as it is saved.
 	FormatOnSaveGoImports FormatOnSave = "goimports"
-)
-
-// GoToDefMode typed constants define the set of valid values that
-// GlobalGoToDefMode can take
-type GoToDefMode string
-
-const (
-	// GoToDefModeTab specifies that CommandGoToDef should open a file in a new
-	// tab when jumping to a definition that does not exist in the current file.
-	GoToDefModeTab GoToDefMode = "tab"
-
-	// GoToDefModeSplit specifies that CommandGoToDef should open a file in a new
-	// split when jumping to a definition that does not exist in the current file.
-	GoToDefModeSplit GoToDefMode = "split"
-
-	// GoToDefModeVsplit specifies that CommandGoToDef should open a file in a
-	// new vertical split when jumping to a definition that does not exist in
-	// the current file.
-	GoToDefModeVsplit GoToDefMode = "vsplit"
 )
