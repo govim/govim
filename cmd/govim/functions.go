6ef0ca512f3e2a0bf7d272d23499432e45ee6b5d
Paul Jolly <paul@myitcv.io>
2019-04-26 01:25:15 +0100

    cmd/govim: move ftplugin callback into separate file
    Makes linking to these defaults easier

--- cmd/govim/functions.go
+++ cmd/govim/functions.go
@@ -548,24 +548,3 @@ func (v *vimstate) deleteCurrentBuffer(args ...json.RawMessage) error {
 	delete(v.buffers, currBufNr)
 	return nil
 }
-
-func (v *vimstate) ftplugin(args ...json.RawMessage) (interface{}, error) {
-	amatch := v.ParseString(args[0])
-	switch amatch {
-	case "go":
-		// Setup buffer-local mappings and settings
-		v.ChannelExf("setlocal balloonexpr=%v%v()", v.Driver.Prefix(), config.FunctionBalloonExpr)
-		v.ChannelExf("setlocal omnifunc=%v%v", v.Driver.Prefix(), config.FunctionComplete)
-		v.ChannelExf("nnoremap <buffer> <silent> <C-]> :%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-		v.ChannelExf("nnoremap <buffer> <silent> gd :%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-		v.ChannelExf("nnoremap <buffer> <silent> <C-]> :%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-		v.ChannelExf("nnoremap <buffer> <silent> <C-LeftMouse> <LeftMouse>:%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-		v.ChannelExf("nnoremap <buffer> <silent> g<LeftMouse> <LeftMouse>:%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-		v.ChannelExf("nnoremap <buffer> <silent> <C-t> :%v%v<cr>", v.Driver.Prefix(), config.CommandGoToPrevDef)
-		v.ChannelExf("nnoremap <buffer> <silent> <C-RightMouse> <RightMouse>:%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-		v.ChannelExf("nnoremap <buffer> <silent> g<RightMouse> <RightMouse>:%v%v<cr>", v.Driver.Prefix(), config.CommandGoToDef)
-	default:
-		return nil, fmt.Errorf("don't yet know how to handle filetype of %v", amatch)
-	}
-	return nil, nil
-}
