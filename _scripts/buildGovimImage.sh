8596305643c8a19655e678853008c808e9acc018
Miles Bryant <milesbryant@monzo.com>
2019-05-05 22:01:43 +0100

    Add CI support for new neovim images (#211)
    -- Splits Dockerfile into base and .vim --
    I think the neovim and vim images should be separate, for ease of
    matrix-testing many different versions. We'd still like to install
    (n)vim onto the same base layer with GO_VERSION and VBASHVERSION, so
    this splits the current Dockerfile into Dockerfile.base and
    Dockerfile.vim.
    
    -- Adds a neovim testing image --

--- _scripts/buildGovimImage.sh
+++ _scripts/buildGovimImage.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env vbash
 
-set -eu
+set -euo pipefail
 
 # Usage; either:
 #
 #   buildGovimImage.sh
-#   buildGovimImage.sh VIMVERSION GOVERSION
+#   buildGovimImage.sh VIMFLAVOR VIMVERSION GOVERSION
 #
 
-if [ "$#" -eq 2 ]
+if [ "$#" -eq 3 ]
 then
-	VIM_VERSION="$1"
-	GO_VERSION="$2"
+    VIM_FLAVOR="$1"
+	VIM_VERSION="$2"
+	GO_VERSION="$3"
 else
+    VIM_FLAVOR="${VIM_FLAVOR:-vim}"
 	VIM_VERSION=$(echo $VIM_VERSIONS | tr ',' '\n' | tail -n 1)
 	GO_VERSION=$(echo $GO_VERSIONS | tr ',' '\n' | tail -n 1)
 fi
 
-cat Dockerfile.user | GO_VERSION=$GO_VERSION VIM_VERSION=$VIM_VERSION envsubst '$GO_VERSION,$VIM_VERSION' | docker build -t govim --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g $USER) -
+
+cat Dockerfile.user \
+    | GO_VERSION=$GO_VERSION VIM_FLAVOR=$VIM_FLAVOR VERSION=$VIM_VERSION envsubst '$GO_VERSION,$VIM_FLAVOR,$VERSION' \
+    | docker build -t govim --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g $USER) -
--- _scripts/rebuildDockerImages.sh
+++ _scripts/rebuildDockerImages.sh
@@ -5,14 +5,15 @@ set -eu
 
 # Usage; either:
 #
-#   rebuildDockerImsages.sh
-#   rebuildDockerImsages.sh VIMVERSION GOVERSION
+#   rebuildDockerImages.sh
+#   rebuildDockerImages.sh VIMVERSION GOVERSION NEOVIMVERSION
 #
 
-if [ "$#" -eq 2 ]
+if [ "$#" -eq 3 ]
 then
 	VIM_VERSIONS="$1"
 	GO_VERSIONS="$2"
+	NEOVIM_VERSIONS="$3"
 else
 	if [ "${VIM_VERSIONS:-}" == "" ]
 	then
@@ -22,15 +23,36 @@ else
 	then
 		GO_VERSIONS="$GO_VERSION"
 	fi
+	if [ "${NEOVIM_VERSIONS:-}" == "" ]
+	then
+		NEOVIM_VERSIONS="$NEOVIM_VERSION"
+	fi
 fi
 
 vbashVersion="$(go list -m -f={{.Version}} github.com/myitcv/vbash)"
 
+# Build base layer image
+echo running docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=${GO_VERSION}" -t govim/govim:base_${GO_VERSION}_${vbashVersion} -f Dockerfile.base .
+docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=${GO_VERSION}" -t govim/govim:base_${GO_VERSION}_${vbashVersion} -f Dockerfile.base .
+
+
+# Build Vim images
 for i in $(echo "$VIM_VERSIONS" | tr ',' '\n')
 do
 	for j in $(echo "$GO_VERSIONS" | tr ',' '\n')
 	do
-		echo running docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=$j" --build-arg "VIM_VERSION=$i" -t govim/govim:${j}_${i} .
-		docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=$j" --build-arg "VIM_VERSION=$i" -t govim/govim:${j}_${i}_v1 .
+		echo running docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=$j" --build-arg "VIM_VERSION=$i" -t govim/govim:${j}_${i} -f Dockerfile.nvim .
+		docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=$j" --build-arg "VIM_VERSION=$i" -t govim/govim:${j}_vim_${i}_v1 -f Dockerfile.vim .
+	done
+done
+
+
+# Build Neovim images
+for i in $(echo "$NEOVIM_VERSIONS" | tr ',' '\n')
+do
+	for j in $(echo "$GO_VERSIONS" | tr ',' '\n')
+	do
+		echo running docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=$j" --build-arg "VIM_VERSION=$i" -t govim/govim:${j}_${i} -f Dockerfile.nvim .
+		docker build --build-arg "VBASHVERSION=$vbashVersion" --build-arg "GO_VERSION=$j" --build-arg "NEOVIM_VERSION=$i" -t govim/govim:${j}_nvim_${i}_v1 -f Dockerfile.nvim .
 	done
 done
