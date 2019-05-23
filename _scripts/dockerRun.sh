b283fa65a5467762e6cfdcf15bc182ae5e60c037
Paul Jolly <paul@myitcv.io>
2019-05-23 09:29:31 +0100

    ci: slim down docker run when not actually running in CI (#243)

--- _scripts/dockerRun.sh
+++ _scripts/dockerRun.sh
@@ -13,15 +13,23 @@ go install golang.org/x/tools/cmd/gopls
 # remove all generated files to ensure we are never stale
 rm -f $(git ls-files -- ':!:cmd/govim/internal' '**/gen_*.go' 'gen_*.go')
 
-go generate ./...
-go test ./...
-go test -race ./...
+go generate $(go list ./... | grep -v 'govim/internal')
+go test $(go list ./... | grep -v 'govim/internal')
+
+if [ "${CI:-}" == "true" ]
+then
+	go test -race $(go list ./... | grep -v 'govim/internal')
+fi
+
 go vet $(go list ./... | grep -v 'govim/internal')
 go run honnef.co/go/tools/cmd/staticcheck $(go list ./... | grep -v 'govim/internal')
 
-go mod tidy
-# https://github.com/golang/go/issues/27868#issuecomment-431413621
-go list all > /dev/null
+if [ "${CI:-}" == "true" ]
+then
+	go mod tidy
+	# https://github.com/golang/go/issues/27868#issuecomment-431413621
+	go list all > /dev/null
 
-diff <(echo -n) <(gofmt -d $(git ls-files '**/*.go' '*.go' | grep -v cmd/govim/internal))
-test -z "$(git status --porcelain)" || (git status; git diff; false)
+	diff <(echo -n) <(gofmt -d $(git ls-files '**/*.go' '*.go' | grep -v cmd/govim/internal))
+	test -z "$(git status --porcelain)" || (git status; git diff; false)
+fi
