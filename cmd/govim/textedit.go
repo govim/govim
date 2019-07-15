2bddfc14771ed224213b0f3a7bcc76960a077a13
Paul Jolly <paul@myitcv.io>
2019-07-15 20:28:49 +0100

    cmd/govim: properly implement handling of []protocol.TextEdit
    This includes support for intra-line changes. As part of this change we
    also factor out the code into a separate method, in preparation for
    upcoming rename support

---
+++ cmd/govim/textedit.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/myitcv/govim/cmd/govim/config"
+	"github.com/myitcv/govim/cmd/govim/internal/lsp/protocol"
+	"github.com/myitcv/govim/cmd/govim/types"
+)
+
+type textEdit struct {
+	buffer int
+	call   string
+	start  int
+	end    int
+	lines  []string
+}
+
+func (v *vimstate) applyProtocolTextEdits(b *types.Buffer, edits []protocol.TextEdit) error {
+
+	// prepare the changes to make in Vim
+	blines := bytes.Split(b.Contents()[:len(b.Contents())-1], []byte("\n"))
+	var changes []textEdit
+	for ie := len(edits) - 1; ie >= 0; ie-- {
+		e := edits[ie]
+		start, err := types.PointFromPosition(b, e.Range.Start)
+		if err != nil {
+			return fmt.Errorf("failed to derive start point from position: %v", err)
+		}
+		end, err := types.PointFromPosition(b, e.Range.End)
+		if err != nil {
+			return fmt.Errorf("failed to derive end point from position: %v", err)
+		}
+		newLines := strings.Split(e.NewText, "\n")
+		appendAdjust := 1
+		if start.Line()-1 < len(blines) {
+			appendAdjust = 0
+			startLine := blines[start.Line()-1]
+			newLines[0] = string(startLine[:start.Col()-1]) + newLines[0]
+			if end.Line()-1 < len(blines) {
+				endLine := blines[end.Line()-1]
+				newLines[len(newLines)-1] += string(endLine[end.Col()-1:])
+			}
+			// we only need to update the start line because we can't have
+			// overlapping edits
+			blines[start.Line()-1] = []byte(newLines[0])
+			changes = append(changes, textEdit{
+				call:   "setbufline",
+				buffer: b.Num,
+				start:  start.Line(),
+				lines:  []string{string(blines[start.Line()-1])},
+			})
+		} else {
+			blines = append(blines, []byte(newLines[0]))
+		}
+		if start.Line() != end.Line() {
+			changes = append(changes, textEdit{
+				call:   "deletebufline",
+				buffer: b.Num,
+				start:  start.Line() + 1,
+				end:    end.Line(),
+			})
+		}
+		if len(newLines) > 1 {
+			changes = append(changes, textEdit{
+				call:   "appendbufline",
+				buffer: b.Num,
+				start:  start.Line() - appendAdjust,
+				lines:  newLines[1-appendAdjust : len(newLines)-appendAdjust],
+			})
+		}
+	}
+
+	// see :help wundo. The use of wundo! is significant. It first deletes
+	// the temp file we created, but only recreates it if there is something
+	// to write.  This is inherently racey... because theorectically the file
+	// might in the meantime have been created by another instance of
+	// govim.... We reduce that risk using the time above
+	tf, err := ioutil.TempFile("", strconv.FormatInt(time.Now().UnixNano(), 10))
+	if err != nil {
+		return fmt.Errorf("failed to create temp undo file")
+	}
+
+	v.ChannelExf("wundo! %v", tf.Name())
+	defer func() {
+		if _, err := os.Stat(tf.Name()); err != nil {
+			return
+		}
+		v.ChannelExf("silent! rundo %v", tf.Name())
+		err = os.Remove(tf.Name())
+	}()
+
+	preEventIgnore := v.ParseString(v.ChannelExpr("&eventignore"))
+	v.ChannelEx("set eventignore=all")
+	defer v.ChannelExf("set eventignore=%v", preEventIgnore)
+	if v.doIncrementalSync() {
+		v.ChannelCall("listener_remove", b.Listener)
+		defer func() {
+			b.Listener = v.ParseInt(v.ChannelCall("listener_add", v.Prefix()+string(config.FunctionEnrichDelta), b.Num))
+		}()
+	}
+	v.BatchStart()
+	for _, e := range changes {
+		switch e.call {
+		case "setbufline":
+			v.BatchAssertChannelCall(AssertIsZero, e.call, b.Num, e.start, e.lines[0])
+		case "deletebufline":
+			v.BatchAssertChannelCall(AssertIsZero, e.call, b.Num, e.start, e.end)
+		case "appendbufline":
+			v.BatchAssertChannelCall(AssertIsZero, e.call, b.Num, e.start, e.lines)
+		default:
+			panic(fmt.Errorf("unknown change type: %v", e.call))
+		}
+	}
+	if v.doIncrementalSync() {
+		v.BatchAssertChannelCall(AssertIsZero, "listener_flush", b.Num)
+	}
+	newContentsRes := v.BatchChannelExprf(`join(getbufline(%v, 0, "$"), "\n")."\n"`, b.Num)
+	v.BatchEnd()
+
+	var newContents string
+	v.Parse(newContentsRes(), &newContents)
+	b.SetContents([]byte(newContents))
+	b.Version++
+	params := &protocol.DidChangeTextDocumentParams{
+		TextDocument: protocol.VersionedTextDocumentIdentifier{
+			TextDocumentIdentifier: b.ToTextDocumentIdentifier(),
+			Version:                float64(b.Version),
+		},
+		ContentChanges: []protocol.TextDocumentContentChangeEvent{
+			{
+				Text: newContents,
+			},
+		},
+	}
+	return v.server.DidChange(context.Background(), params)
+}
