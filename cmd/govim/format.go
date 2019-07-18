cf708d9749655c60c7ef370b904f244ad40bb890
Paul Jolly <paul@myitcv.io>
2019-07-18 21:36:18 +0100

    cmd/govim: properly implement handling of []protocol.TextEdit (#376)
    This includes support for intra-line changes. As part of this change we
    also factor out the code into a separate method, in preparation for
    upcoming rename support

--- cmd/govim/format.go
+++ cmd/govim/format.go
@@ -4,11 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/myitcv/govim"
 	"github.com/myitcv/govim/cmd/govim/config"
@@ -118,123 +113,8 @@ func (v *vimstate) formatBufferRange(b *types.Buffer, mode config.FormatOnSave, 
 	default:
 		return fmt.Errorf("unknown format mode specified: %v", mode)
 	}
-
 	if len(edits) == 0 {
 		return nil
 	}
-
-	// see :help wundo. The use of wundo! is significant. It first deletes
-	// the temp file we created, but only recreates it if there is something
-	// to write.  This is inherently racey... because theorectically the file
-	// might in the meantime have been created by another instance of
-	// govim.... We reduce that risk using the time above
-	tf, err := ioutil.TempFile("", strconv.FormatInt(time.Now().UnixNano(), 10))
-	if err != nil {
-		return fmt.Errorf("failed to create temp undo file")
-	}
-
-	v.ChannelExf("wundo! %v", tf.Name())
-	defer func() {
-		if _, err := os.Stat(tf.Name()); err != nil {
-			return
-		}
-		v.ChannelExf("silent! rundo %v", tf.Name())
-		err = os.Remove(tf.Name())
-	}()
-
-	// prepare the changes to make in Vim
-	var changes []textEdit
-	for ie := len(edits) - 1; ie >= 0; ie-- {
-		e := edits[ie]
-		start, err := types.PointFromPosition(b, e.Range.Start)
-		if err != nil {
-			return fmt.Errorf("failed to derive start point from position: %v", err)
-		}
-		end, err := types.PointFromPosition(b, e.Range.End)
-		if err != nil {
-			return fmt.Errorf("failed to derive end point from position: %v", err)
-		}
-
-		if start.Col() != 1 || end.Col() != 1 {
-			// Whether this is a delete or not, we will implement support for this later
-			return fmt.Errorf("saw an edit where start col != end col (range start: %v, range end: %v start: %v, end: %v). We can't currently handle this", e.Range.Start, e.Range.End, start, end)
-		}
-
-		if start.Line() != end.Line() {
-			if e.NewText != "" {
-				return fmt.Errorf("saw an edit where start line != end line with replacement text %q; We can't currently handle this", e.NewText)
-			}
-			changes = append(changes, textEdit{
-				call:   "deletebufline",
-				buffer: b.Num,
-				start:  start.Line(),
-				end:    end.Line() - 1,
-			})
-		} else {
-			// do we have anything to do?
-			if e.NewText == "" {
-				continue
-			}
-			// we are within the same line so strip the newline
-			if e.NewText[len(e.NewText)-1] == '\n' {
-				e.NewText = e.NewText[:len(e.NewText)-1]
-			}
-			repl := strings.Split(e.NewText, "\n")
-			changes = append(changes, textEdit{
-				call:   "appendbufline",
-				buffer: b.Num,
-				start:  start.Line() - 1,
-				lines:  repl,
-			})
-		}
-	}
-
-	preEventIgnore := v.ParseString(v.ChannelExpr("&eventignore"))
-	v.ChannelEx("set eventignore=all")
-	defer v.ChannelExf("set eventignore=%v", preEventIgnore)
-	if v.doIncrementalSync() {
-		v.ChannelCall("listener_remove", b.Listener)
-		defer func() {
-			b.Listener = v.ParseInt(v.ChannelCall("listener_add", v.Prefix()+string(config.FunctionEnrichDelta), b.Num))
-		}()
-	}
-	v.BatchStart()
-	for _, e := range changes {
-		switch e.call {
-		case "deletebufline":
-			v.BatchAssertChannelCall(AssertIsZero, "deletebufline", b.Num, e.start, e.end)
-		case "appendbufline":
-			v.BatchAssertChannelCall(AssertIsZero, "appendbufline", b.Num, e.start, e.lines)
-		}
-	}
-	if v.doIncrementalSync() {
-		v.BatchAssertChannelCall(AssertIsZero, "listener_flush", b.Num)
-	}
-	newContentsRes := v.BatchChannelExprf(`join(getbufline(%v, 0, "$"), "\n")."\n"`, b.Num)
-	v.BatchEnd()
-
-	var newContents string
-	v.Parse(newContentsRes(), &newContents)
-	b.SetContents([]byte(newContents))
-	b.Version++
-	params := &protocol.DidChangeTextDocumentParams{
-		TextDocument: protocol.VersionedTextDocumentIdentifier{
-			TextDocumentIdentifier: b.ToTextDocumentIdentifier(),
-			Version:                float64(b.Version),
-		},
-		ContentChanges: []protocol.TextDocumentContentChangeEvent{
-			{
-				Text: newContents,
-			},
-		},
-	}
-	return v.server.DidChange(context.Background(), params)
-}
-
-type textEdit struct {
-	buffer int
-	call   string
-	start  int
-	end    int
-	lines  []string
+	return v.applyProtocolTextEdits(b, edits)
 }
