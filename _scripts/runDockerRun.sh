d7323638fd31ae9817b0ae1bf689ed139b95c66c
Pontus Leitzler <leitzler@gmail.com>
2019-07-24 21:21:55 +0000

    fix GOPATH in runDockerRun.sh

--- _scripts/runDockerRun.sh
+++ _scripts/runDockerRun.sh
@@ -10,7 +10,7 @@ proxy=""
 
 if [ "${CI:-}" != "true" ]
 then
-	proxy="-v $GOPATH/pkg/mod/cache/download:/cache -e GOPROXY=file:///cache"
+	proxy="-v $(go env GOPATH)/pkg/mod/cache/download:/cache -e GOPROXY=file:///cache"
 fi
 
 docker run $proxy --env-file ./_scripts/.docker_env_file -e "VIM_FLAVOR=${VIM_FLAVOR:-vim}" -v $PWD:/home/$USER/govim -w /home/$USER/govim --rm govim ./_scripts/dockerRun.sh
