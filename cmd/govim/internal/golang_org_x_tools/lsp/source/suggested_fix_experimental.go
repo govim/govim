8335246b5079b0e31b0842f22d72d677b149ce3e
Paul Jolly <paul@myitcv.io>
2019-07-29 15:38:01 +0100

    deps: x/tools and gopls ff9f1409240a
    * internal/lsp/source: fix renaming of SuggestedFixes in experimental file ff9f1409
    * internal/lsp: process configuration per workspace folder fc6e2057

--- cmd/govim/internal/golang_org_x_tools/lsp/source/suggested_fix_experimental.go
+++ cmd/govim/internal/golang_org_x_tools/lsp/source/suggested_fix_experimental.go
@@ -8,10 +8,10 @@ import (
 	"github.com/myitcv/govim/cmd/govim/internal/golang_org_x_tools/span"
 )
 
-func getCodeActions(fset *token.FileSet, diag analysis.Diagnostic) ([]CodeAction, error) {
-	var cas []CodeAction
+func getCodeActions(fset *token.FileSet, diag analysis.Diagnostic) ([]SuggestedFixes, error) {
+	var cas []SuggestedFixes
 	for _, fix := range diag.SuggestedFixes {
-		var ca CodeAction
+		var ca SuggestedFixes
 		ca.Title = fix.Message
 		for _, te := range fix.TextEdits {
 			span, err := span.NewRange(fset, te.Pos, te.End).Span()
