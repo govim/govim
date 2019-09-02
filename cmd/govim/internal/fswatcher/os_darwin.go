76da245e63deae6a258d009715a9b67f9465b770
Pontus Leitzler <leitzler@gmail.com>
2019-09-02 13:41:42 +0200

    cmd/govim: add separate watcher for darwin

---
+++ cmd/govim/internal/fswatcher/os_darwin.go
@@ -0,0 +1,91 @@
+// +build darwin
+
+package fswatcher
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsevents"
+	"gopkg.in/tomb.v2"
+)
+
+const fRemoved = fsevents.ItemRemoved | fsevents.ItemRenamed
+const fChanged = fsevents.ItemCreated | fsevents.ItemModified | fsevents.ItemChangeOwner
+
+type fswatcher struct {
+	eventCh chan Event
+	es      *fsevents.EventStream
+}
+
+func New(gomodpath string, tomb *tomb.Tomb) (*FSWatcher, error) {
+	dirpath := filepath.Dir(gomodpath)
+	dev, err := fsevents.DeviceForPath(dirpath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve device for path %v: %v", dirpath, err)
+	}
+
+	es := &fsevents.EventStream{
+		Paths:   []string{dirpath},
+		Latency: 200 * time.Millisecond,
+		Device:  dev,
+		Flags:   fsevents.FileEvents | fsevents.WatchRoot,
+	}
+
+	es.Start()
+
+	// fsevents returns paths relative to device root so we need
+	// to figure out the actual mount point
+	mountPoint, err := filepath.Abs(dirpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for mountPoint != string(os.PathSeparator) {
+		parent := filepath.Dir(mountPoint)
+		pDev, err := fsevents.DeviceForPath(parent)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if pDev != dev {
+			break
+		}
+		mountPoint = parent
+	}
+
+	eventCh := make(chan Event)
+	tomb.Go(func() error {
+		for {
+			events, ok := <-es.Events
+			if !ok {
+				break
+			}
+			for i := range events {
+				event := events[i]
+				path := filepath.Join(mountPoint, event.Path)
+				switch {
+				case event.Flags&fRemoved > 0:
+					eventCh <- Event{path, OpRemoved}
+				case event.Flags&fChanged > 0:
+					eventCh <- Event{path, OpChanged}
+				}
+			}
+		}
+		close(eventCh)
+		return nil
+	})
+
+	return &FSWatcher{&fswatcher{eventCh, es}}, nil
+}
+
+func (w *fswatcher) Add(path string) error    { return nil }
+func (w *fswatcher) Remove(path string) error { return nil }
+func (w *fswatcher) Close() error {
+	w.es.Stop()
+	return nil
+}
+func (w *fswatcher) Events() chan Event { return w.eventCh }
+func (w *fswatcher) Errors() chan error { return make(chan error) }
