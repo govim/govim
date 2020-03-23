392db4fea92e1d63505011bb8ffc42cce76e5630
Tom Cain <tom@tjcain.com>
2020-03-23 13:07:29 +0000

    cmd/govim: implement Implements in gopls
    This change adds the command GOVIMImplements which shows all
    interfaces that the identifier under the cursor implements.
    
    Fixes #577

--- cmd/govim/util.go
+++ cmd/govim/util.go
@@ -3,7 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+	"strings"
 
+	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/lsp/protocol"
+	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/span"
 	"github.com/govim/govim/cmd/govim/internal/types"
 )
 
@@ -43,4 +49,83 @@ func (v *vimstate) cursorPos() (b *types.Buffer, p types.Point, err error) {
 	}
 	p, err = types.PointFromVim(b, pos.Line, pos.Col)
 	return
+}
+
+// populateQuickfix populates and opens a quickfix window with a sorted
+// slice of locations. If shift is true the first element of the slice
+// will be skipped.
+func (v *vimstate) populateQuickfix(locs []protocol.Location, shift bool) {
+	// must be non-nil
+	qf := []quickfixEntry{}
+
+	for _, loc := range locs {
+		var buf *types.Buffer
+		for _, b := range v.buffers {
+			if b.Loaded && b.URI() == span.URI(loc.URI) {
+				buf = b
+			}
+		}
+		fn := span.URI(loc.URI).Filename()
+		if buf == nil {
+			byts, err := ioutil.ReadFile(fn)
+			if err != nil {
+				v.Logf("populateQuickfix: failed to read contents of %v: %v", fn, err)
+				continue
+			}
+			// create a temp buffer
+			buf = types.NewBuffer(-1, fn, byts, false)
+		}
+		// make fn relative for reporting purposes
+		fn, err := filepath.Rel(v.workingDirectory, fn)
+		if err != nil {
+			v.Logf("populateQuickfix: failed to call filepath.Rel(%q, %q): %v", v.workingDirectory, fn, err)
+			continue
+		}
+		p, err := types.PointFromPosition(buf, loc.Range.Start)
+		if err != nil {
+			v.Logf("popularQuickfix: failed to resolve position: %v", err)
+			continue
+		}
+		line, err := buf.Line(p.Line())
+		if err != nil {
+			v.Logf("popularQuickfix: location invalid in buffer: %v", err)
+			continue
+		}
+		qf = append(qf, quickfixEntry{
+			Filename: fn,
+			Lnum:     p.Line(),
+			Col:      p.Col(),
+			Text:     line,
+		})
+	}
+
+	var toSort []quickfixEntry
+
+	if shift {
+		toSort = qf[1:]
+	} else {
+		toSort = qf
+	}
+
+	sort.Slice(toSort, func(i, j int) bool {
+		lhs, rhs := toSort[i], toSort[j]
+		cmp := strings.Compare(lhs.Filename, rhs.Filename)
+		if cmp != 0 {
+			if lhs.Filename == qf[0].Filename {
+				return true
+			} else if rhs.Filename == qf[0].Filename {
+				return false
+			}
+		}
+		if cmp == 0 {
+			cmp = lhs.Lnum - rhs.Lnum
+		}
+		if cmp == 0 {
+			cmp = lhs.Col - rhs.Col
+		}
+		return cmp < 0
+	})
+
+	v.ChannelCall("setqflist", qf, "r")
+	v.ChannelEx("copen")
 }
