d035992a9052af9185906c3b2c4cc64b80df3c7e
Paul Jolly <paul@myitcv.io>
2020-04-07 19:11:24 +0100

    deps: update x/tools and gopls to a3568bac (#849)
    * internal/lsp: disable unimported completions for snippet tests a3568bac
    * internal/telemetry/event: fix error/value key type tag formatting 066fd139
    * internal/lsp: make sure that `gofmt -s` analyses don't modify AST 1fd97665
    * internal/lsp: make event directly implement TagMap 903869a8
    * internal/telemetry: add an example of using the logging behaviour ff0df582
    * internal/lsp: use one context throughout completion ee2abff5
    * internal/lsp: rewrite the rpc debug page 7db14c95
    * internal/jsonrpc2: dont add any handlers by default ccaaa5c2
    * internal/jsonrpc2: remove the legacy interface 17cc17e0
    * internal/jsonrpc2: break Run up into composable handlers 3eebf4bf
    * internal/jsonrpc2: remove request state 5c4bdbc0

--- cmd/govim/internal/golang_org_x_tools/jsonrpc2/handler.go
+++ cmd/govim/internal/golang_org_x_tools/jsonrpc2/handler.go
@@ -7,6 +7,9 @@ package jsonrpc2
 import (
 	"context"
 	"fmt"
+	"sync"
+
+	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/telemetry/event"
 )
 
 // Handler is invoked to handle incoming requests.
@@ -14,31 +17,6 @@ import (
 // call Reply on the Conn with the supplied request.
 // The handler should return ErrNotHandled if it could not handle the request.
 type Handler func(context.Context, *Request) error
-
-// LegacyHooks is a temporary measure during migration from the old Handler
-// interface to the new HandleFunc.
-// The intent is to delete this interface in a later cl.
-type LegacyHooks interface {
-	// Deliver is invoked to handle incoming requests.
-	// If the request returns false from IsNotify then the Handler must eventually
-	// call Reply on the Conn with the supplied request.
-	// Handlers are called synchronously, they should pass the work off to a go
-	// routine if they are going to take a long time.
-	// If Deliver returns true all subsequent handlers will be invoked with
-	// delivered set to true, and should not attempt to deliver the message.
-	Deliver(ctx context.Context, r *Request, delivered bool) bool
-
-	// Cancel is invoked for cancelled outgoing requests.
-	// It is okay to use the connection to send notifications, but the context will
-	// be in the cancelled state, so you must do it with the background context
-	// instead.
-	// If Cancel returns true all subsequent handlers will be invoked with
-	// cancelled set to true, and should not attempt to cancel the message.
-	Cancel(ctx context.Context, conn *Conn, id ID, cancelled bool) bool
-
-	// Request is called near the start of processing any request.
-	Request(ctx context.Context, conn *Conn, direction Direction, r *WireRequest) context.Context
-}
 
 // Direction is used to indicate to a logger whether the logged message was being
 // sent or received.
@@ -74,9 +52,65 @@ func MethodNotFound(ctx context.Context, r *Request) error {
 func MustReply(handler Handler) Handler {
 	return func(ctx context.Context, req *Request) error {
 		err := handler(ctx, req)
-		if req.state < requestReplied {
+		if req.done != nil {
 			panic(fmt.Errorf("request %q was never replied to", req.Method))
 		}
 		return err
+	}
+}
+
+// CancelHandler returns a handler that supports cancellation, and a canceller
+// that can be used to trigger canceling in progress requests.
+func CancelHandler(handler Handler) (Handler, Canceller) {
+	var mu sync.Mutex
+	handling := make(map[ID]context.CancelFunc)
+	wrapped := func(ctx context.Context, req *Request) error {
+		if req.ID != nil {
+			cancelCtx, cancel := context.WithCancel(ctx)
+			ctx = cancelCtx
+			mu.Lock()
+			handling[*req.ID] = cancel
+			mu.Unlock()
+			req.OnReply(func() {
+				mu.Lock()
+				delete(handling, *req.ID)
+				mu.Unlock()
+			})
+		}
+		return handler(ctx, req)
+	}
+	return wrapped, func(id ID) {
+		mu.Lock()
+		cancel, found := handling[id]
+		mu.Unlock()
+		if found {
+			cancel()
+		}
+	}
+}
+
+// AsyncHandler returns a handler that processes each request goes in its own
+// goroutine.
+// The handler returns immediately, without the request being processed.
+// Each request then waits for the previous request to finish before it starts.
+// This allows the stream to unblock at the cost of unbounded goroutines
+// all stalled on the previous one.
+func AsyncHandler(handler Handler) Handler {
+	nextRequest := make(chan struct{})
+	close(nextRequest)
+	return func(ctx context.Context, req *Request) error {
+		waitForPrevious := nextRequest
+		nextRequest = make(chan struct{})
+		unlockNext := nextRequest
+		req.OnReply(func() { close(unlockNext) })
+		_, queueDone := event.StartSpan(ctx, "queued")
+		go func() {
+			<-waitForPrevious
+			queueDone()
+			if err := handler(ctx, req); err != nil {
+				event.Error(ctx, "jsonrpc2 async message delivery failed", err)
+			}
+		}()
+		return nil
 	}
 }
