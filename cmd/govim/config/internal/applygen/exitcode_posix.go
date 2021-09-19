2424e817df41eec641c4b70cf61fe44055934d51
Pontus Leitzler <leitzler@gmail.com>
2021-09-19 19:11:20 +0000

    ci: update go and vim versions
    Update go versions ["go1.15.8", "go1.16"] to ["go1.16.8", "go1.17.1"]
    Update latest vim version v8.2.3249 to v8.2.3450
    
    This change also adds the "new style" build tags to files using build
    tags, since goimports will add them automatically from go 1.17.

--- cmd/govim/config/internal/applygen/exitcode_posix.go
+++ cmd/govim/config/internal/applygen/exitcode_posix.go
@@ -1,3 +1,4 @@
+//go:build darwin || dragonfly || freebsd || (js && wasm) || linux || nacl || netbsd || openbsd || solaris || windows
 // +build darwin dragonfly freebsd js,wasm linux nacl netbsd openbsd solaris windows
 
 package main
