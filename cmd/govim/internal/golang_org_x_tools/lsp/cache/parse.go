d01464d53ae44578bdc5f84da10ca8936be9b551
Pontus Leitzler <leitzler@gmail.com>
2022-06-28 12:15:50 +0000

    deps: update x/tools and gopls to 7404bd2ff
    * all: gofmt some recent file changes 7404bd2ff
    * refactor/satisfy: don't crash on type parameters ec0831a43
    * internal/memoize: remove unused Store.generations map 66bbba3d5
    * internal/memoize: don't destroy reference counted handles 56116ec01
    * cmd/digraph: fix typo 10494c735
    * internal/lsp/cache: invalidate reverse closure when loading packages 93a03c2c5
    * internal/lsp/cache: don't set new metadata if existing is valid c36379be2
    * internal/imports: use a module resolver if GOWORK is set e1ec1f323
    * internal/lsp: use camel case for inlay hint config fields 60ca6366e
    * internal/persistent: change map to use set/get as method names 2994e9941
    * internal/lsp: rename viewport to range 22ab2538d
    * internal/lsp/cache: use persistent map for storing files in the snapshot 3f5f798e2
    * internal/lsp/cache: use persistent map for storing gofiles in the snapshot f60e9bc48
    * internal/lsp: add settings for inlay hints and enable 871637b64
    * cmd/stringer: use explicit NeedX values instead of deprecated LoadSyntax a44cc76dc
    * internal/lsp/cache: don't prune unreachable metadata on clone 4e231cb6f
    * internal/lsp/cache: honor the go.work for computing workspace packages a2de63544
    * internal/span: optimise URI.Filename to avoid allocation cbb8e8e92
    * internal/lsp/cache: minor simplifications to Symbols 63d8015eb
    * internal/lsp: find references to package 59bd4faed
    * internal/lsp: remove tooltip from inlay hints a1303c83f
    * internal/lsp: add inlay hints for inferred type params 641b30b3f
    * go/packages: fix loading single file when outside of GOPATH, module 70ccf57e4

--- cmd/govim/internal/golang_org_x_tools/lsp/cache/parse.go
+++ cmd/govim/internal/golang_org_x_tools/lsp/cache/parse.go
@@ -58,17 +58,17 @@ func (s *snapshot) parseGoHandle(ctx context.Context, fh source.FileHandle, mode
 	if pgh := s.getGoFile(key); pgh != nil {
 		return pgh
 	}
-	parseHandle := s.generation.Bind(key, func(ctx context.Context, arg memoize.Arg) interface{} {
+	parseHandle, release := s.generation.GetHandle(key, func(ctx context.Context, arg memoize.Arg) interface{} {
 		snapshot := arg.(*snapshot)
 		return parseGo(ctx, snapshot.FileSet(), fh, mode)
-	}, nil)
+	})
 
 	pgh := &parseGoHandle{
 		handle: parseHandle,
 		file:   fh,
 		mode:   mode,
 	}
-	return s.addGoFile(key, pgh)
+	return s.addGoFile(key, pgh, release)
 }
 
 func (pgh *parseGoHandle) String() string {
