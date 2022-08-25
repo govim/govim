4876cab35c963e9b40a03797484dfa9a51a978e2
Pontus Leitzler <leitzler@users.noreply.github.com>
2022-08-25 13:08:43 +0200

    deps: update x/tools and gopls to db6a62ca5 (#1166)
    * go/internal/gcimporter: call Interface.Complete in unified importer db6a62ca5
    * internal/lsp: hover to render go 1.19 doc comments 587a15310
    * internal/lsp/cache: clear shouldLoad IDs on load e55fb40e6
    * godoc/redirect: delete golang.org-specific code a3cac1188

--- cmd/govim/internal/golang_org_x_tools/lsp/cmd/test/definition.go
+++ cmd/govim/internal/golang_org_x_tools/lsp/cmd/test/definition.go
@@ -10,8 +10,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/lsp/diff"
-	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/lsp/diff/myers"
 	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/lsp/tests"
 	"github.com/govim/govim/cmd/govim/internal/golang_org_x_tools/span"
 )
@@ -51,11 +49,7 @@ func (r *runner) Definition(t *testing.T, spn span.Span, d tests.Definition) {
 			return []byte(got), nil
 		})))
 		if expect != "" && !strings.HasPrefix(got, expect) {
-			d, err := myers.ComputeEdits("", expect, got)
-			if err != nil {
-				t.Fatal(err)
-			}
-			t.Errorf("definition %v failed with %#v\n%s", tag, args, diff.ToUnified("expect", "got", expect, d))
+			tests.CheckSameMarkdown(t, got, expect)
 		}
 	}
 }
