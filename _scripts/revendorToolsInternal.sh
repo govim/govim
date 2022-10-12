56a0d43246189b685a3628b04f5ab8db33fcfd88
Paul Jolly <paul@myitcv.io>
2022-10-12 10:51:58 +0100

    deps: optimise vendor of golang.org/x/tools(/gopls) internals (#1172)

--- _scripts/revendorToolsInternal.sh
+++ _scripts/revendorToolsInternal.sh
@@ -1,29 +1,65 @@
-#!/usr/bin/env vbash
+#!/usr/bin/env bash
 
 source "${BASH_SOURCE%/*}/common.bash"
 
 shopt -s extglob
 
+# Save location of root of repo and change there to start
+SCRIPT_DIR="$( command cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/..
+cd $SCRIPT_DIR
+
+# Ensure that all dependencies are downloaded and present
+# in the module cache
 go mod download
 
+# Get module cache directories of gopls and tools.
+# Note this gives us the result post replace directives
+# which is what we want.
 tools=$(go list -m -f={{.Dir}} golang.org/x/tools)
 gopls=$(go list -m -f={{.Dir}} golang.org/x/tools/gopls)
 
-echo "Tools is $tools, gopls is $gopls"
+# Establish a temporary module to collect and vendor
+# our internal requirements
+td=$(mktemp -d)
+trap "rm -rf $td" EXIT
+cd $td
+go mod init example.com
+cat <<EOD > deps.go
+package deps
 
-cd $(git rev-parse --show-toplevel)
+import (
+	_ "golang.org/x/tools/gopls/internal/lsp/protocol"
+	_ "golang.org/x/tools/gopls/internal/lsp/source"
+	_ "golang.org/x/tools/gopls/internal/lsp/command"
+	_ "golang.org/x/tools/internal/fakenet"
+	_ "golang.org/x/tools/internal/span"
+	_ "golang.org/x/tools/internal/jsonrpc2"
+)
+EOD
+
+# Add replace directives by hand because go mod edit
+# misinterprets a directory containing an '@' as an
+# indication of the version
+cat <<EOD >> go.mod
+replace golang.org/x/tools => $tools
+replace golang.org/x/tools/gopls => $gopls
+EOD
+go mod tidy
+go mod vendor
+
+cd $SCRIPT_DIR
 
 tools_regex='s+golang.org/x/tools/internal+github.com/govim/govim/cmd/govim/internal/golang_org_x_tools+g'
 gopls_regex='s+golang.org/x/tools/gopls/internal+github.com/govim/govim/cmd/govim/internal/golang_org_x_tools_gopls+g'
 
 if [ $(go env GOHOSTOS) = 'darwin' ]; then
-    rsync -a --delete --chmod=Du+w,Fu+w $tools/internal/ ./cmd/govim/internal/golang_org_x_tools
-    rsync -a --delete --chmod=Du+w,Fu+w $gopls/internal/ ./cmd/govim/internal/golang_org_x_tools_gopls
+    rsync -a --delete --chmod=Du+w,Fu+w $td/vendor/golang.org/x/tools/internal/ ./cmd/govim/internal/golang_org_x_tools
+    rsync -a --delete --chmod=Du+w,Fu+w $td/vendor/golang.org/x/tools/gopls/internal/ ./cmd/govim/internal/golang_org_x_tools_gopls
     find ./cmd/govim/internal/golang_org_x_tools ./cmd/govim/internal/golang_org_x_tools_gopls  -name "*.go" -exec sed -i '' -e $tools_regex {} +
     find ./cmd/govim/internal/golang_org_x_tools_gopls -name "*.go" -exec sed -i '' -e $gopls_regex {} +
 else
-    rsync -a --delete --chmod=D0755,F0644 $tools/internal/ ./cmd/govim/internal/golang_org_x_tools
-    rsync -a --delete --chmod=D0755,F0644 $gopls/internal/ ./cmd/govim/internal/golang_org_x_tools_gopls
+    rsync -a --delete --chmod=D0755,F0644 $td/vendor/golang.org/x/tools/internal/ ./cmd/govim/internal/golang_org_x_tools
+    rsync -a --delete --chmod=D0755,F0644 $td/vendor/golang.org/x/tools/gopls/internal/ ./cmd/govim/internal/golang_org_x_tools_gopls
     find ./cmd/govim/internal/golang_org_x_tools ./cmd/govim/internal/golang_org_x_tools_gopls -name "*.go" -exec sed -i $tools_regex {} +
     find ./cmd/govim/internal/golang_org_x_tools_gopls -name "*.go" -exec sed -i $gopls_regex {} +
 fi
--- go.mod
+++ go.mod
@@ -10,20 +10,15 @@ require (
 	github.com/kr/pretty v0.3.0
 	github.com/myitcv/vbash v0.0.4
 	github.com/rogpeppe/go-internal v1.8.1
-	github.com/sergi/go-diff v1.1.0
 	golang.org/x/exp/typeparams v0.0.0-20220722155223-a9213eeb770e
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
 	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664
 	golang.org/x/text v0.3.7
 	golang.org/x/tools v0.1.13-0.20220915194103-a61f20e1aa17
 	golang.org/x/tools/gopls v0.0.0-20220915194103-a61f20e1aa17
-	golang.org/x/vuln v0.0.0-20220901221904-62b0186a1058
 	gopkg.in/retry.v1 v1.0.3
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637
 	honnef.co/go/tools v0.3.3
-	mvdan.cc/gofumpt v0.3.1
-	mvdan.cc/xurls/v2 v2.4.0
 )
 
 require (
@@ -31,9 +26,14 @@ require (
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pkg/diff v0.0.0-20210226163009-20ebb0f2a09e // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/vuln v0.0.0-20220901221904-62b0186a1058 // indirect
 	gopkg.in/errgo.v2 v2.1.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
+	mvdan.cc/gofumpt v0.3.1 // indirect
 	mvdan.cc/sh/v3 v3.0.0-alpha1 // indirect
+	mvdan.cc/xurls/v2 v2.4.0 // indirect
 )
