8e2cd0cb09d8277521208ddee622b8c0b6cf1421
Pontus Leitzler <leitzler@gmail.com>
2023-09-28 23:05:43 +0200

    deps: update x/tools and gopls to 451716b51
    * internal/refactor/inline: consider "", 0.0, 1.0 duplicable 451716b51
    * internal/refactor/inline: tweak everything test for cgo 792f91f70
    * internal/refactor/inline: fix pkgname shadowing bug 941629930
    * gopls/internal/lsp/fake: don't set a completion budget for tests 4cd12d6da
    * gopls/internal/lsp: hover over embed directives 57ecf488e
    * gopls/internal/lsp/cache: remove snapshot.typeCheckMu 6de34480a
    * gopls/internal/regtest/source/completion: reuse functionCallSnippet in unimported completion 7f23bc81d
    * gopls/internal/lsp: use matcher score in ranking unimported candidates 3d03fbd05
    * gopls/internal/lsp/source: don't complete to golang.org/toolchain 7577387a8

--- cmd/govim/internal/golang_org_x_tools/refactor/inline/inline.go
+++ cmd/govim/internal/golang_org_x_tools/refactor/inline/inline.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"fmt"
 	"go/ast"
+	"go/constant"
 	"go/format"
 	"go/parser"
 	"go/token"
@@ -291,14 +292,17 @@ func inline(logf func(string, ...any), caller *Caller, callee *gobCallee) (*resu
 
 	// localImportName returns the local name for a given imported package path.
 	var newImports []*ast.ImportSpec
-	localImportName := func(path string) string {
+	localImportName := func(path string, shadows map[string]bool) string {
 		// Does an import exist?
 		for _, name := range importMap[path] {
 			// Check that either the import preexisted,
-			// or that it was newly added (no PkgName) but is not shadowed.
-			found := caller.lookup(name)
-			if is[*types.PkgName](found) || found == nil {
-				return name
+			// or that it was newly added (no PkgName) but is not shadowed,
+			// either in the callee (shadows) or caller (caller.lookup).
+			if !shadows[name] {
+				found := caller.lookup(name)
+				if is[*types.PkgName](found) || found == nil {
+					return name
+				}
 			}
 		}
 
@@ -313,7 +317,7 @@ func inline(logf func(string, ...any), caller *Caller, callee *gobCallee) (*resu
 		// use the package's declared name.
 		base := pathpkg.Base(path)
 		name := base
-		for n := 0; caller.lookup(name) != nil; n++ {
+		for n := 0; shadows[name] || caller.lookup(name) != nil; n++ {
 			name = fmt.Sprintf("%s%d", base, n)
 		}
 
@@ -344,7 +348,7 @@ func inline(logf func(string, ...any), caller *Caller, callee *gobCallee) (*resu
 		//   => check not shadowed in caller.
 		// - package-level var/func/const/types
 		//   => same package: check not shadowed in caller.
-		//   => otherwise: import other package form a qualified identifier.
+		//   => otherwise: import other package, form a qualified identifier.
 		//      (Unexported cross-package references were rejected already.)
 		// - type parameter
 		//   => not yet supported
@@ -353,10 +357,14 @@ func inline(logf func(string, ...any), caller *Caller, callee *gobCallee) (*resu
 		//
 		// There can be no free references to labels, fields, or methods.
 
+		// Note that we must consider potential shadowing both
+		// at the caller side (caller.lookup) and, when
+		// choosing new PkgNames, within the callee (obj.shadow).
+
 		var newName ast.Expr
 		if obj.Kind == "pkgname" {
 			// Use locally appropriate import, creating as needed.
-			newName = makeIdent(localImportName(obj.PkgPath)) // imported package
+			newName = makeIdent(localImportName(obj.PkgPath, obj.Shadow)) // imported package
 
 		} else if !obj.ValidPos {
 			// Built-in function, type, or value (e.g. nil, zero):
@@ -396,7 +404,7 @@ func inline(logf func(string, ...any), caller *Caller, callee *gobCallee) (*resu
 
 			// Form a qualified identifier, pkg.Name.
 			if qualify {
-				pkgName := localImportName(obj.PkgPath)
+				pkgName := localImportName(obj.PkgPath, obj.Shadow)
 				newName = &ast.SelectorExpr{
 					X:   makeIdent(pkgName),
 					Sel: makeIdent(obj.Name),
@@ -859,7 +867,7 @@ func inline(logf func(string, ...any), caller *Caller, callee *gobCallee) (*resu
 		return res, nil
 	}
 
-	// TODO(adonovan): parameterless call to { stmt; return expr }
+	// TODO(adonovan): parameterless call to { stmts; return expr }
 	// from one of these contexts:
 	//    x, y     = f()
 	//    x, y    := f()
@@ -1809,17 +1817,30 @@ func duplicable(info *types.Info, e ast.Expr) bool {
 	switch e := e.(type) {
 	case *ast.ParenExpr:
 		return duplicable(info, e.X)
+
 	case *ast.Ident:
 		return true
+
 	case *ast.BasicLit:
-		return e.Kind == token.INT
+		v := info.Types[e].Value
+		switch e.Kind {
+		case token.INT:
+			return true // any int
+		case token.STRING:
+			return consteq(v, kZeroString) // only ""
+		case token.FLOAT:
+			return consteq(v, kZeroFloat) || consteq(v, kOneFloat) // only 0.0 or 1.0
+		}
+
 	case *ast.UnaryExpr: // e.g. +1, -1
 		return (e.Op == token.ADD || e.Op == token.SUB) && duplicable(info, e.X)
+
 	case *ast.CallExpr:
 		// Don't treat a conversion T(x) as duplicable even
 		// if x is duplicable because it could duplicate
 		// allocations. There may be cases to tease apart here.
 		return false
+
 	case *ast.SelectorExpr:
 		if sel, ok := info.Selections[e]; ok {
 			// A field or method selection x.f is referentially
@@ -1828,10 +1849,20 @@ func duplicable(info *types.Info, e ast.Expr) bool {
 		}
 		// A qualified identifier pkg.Name is referentially transparent.
 		return true
-	default:
-		return false
 	}
+	return false
 }
+
+func consteq(x, y constant.Value) bool {
+	return constant.Compare(x, token.EQL, y)
+}
+
+var (
+	kZeroInt    = constant.MakeInt64(0)
+	kZeroString = constant.MakeString("")
+	kZeroFloat  = constant.MakeFloat64(0.0)
+	kOneFloat   = constant.MakeFloat64(1.0)
+)
 
 // -- inline helpers --
 
--- go.mod
+++ go.mod
@@ -16,8 +16,8 @@ require (
 	golang.org/x/sys v0.12.0
 	golang.org/x/telemetry v0.0.0-20230923135512-f45a5404d02c
 	golang.org/x/text v0.13.0
-	golang.org/x/tools v0.13.1-0.20230927183407-4b34fbf5f10d
-	golang.org/x/tools/gopls v0.0.0-20230927183407-4b34fbf5f10d
+	golang.org/x/tools v0.13.1-0.20230928202826-451716b51585
+	golang.org/x/tools/gopls v0.0.0-20230928202826-451716b51585
 	gopkg.in/retry.v1 v1.0.3
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637
 	honnef.co/go/tools v0.4.5
